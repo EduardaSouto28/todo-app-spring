a304c69ebd72e842b8956cc5468a85f96882243e
Eduarda Souto <101151329+EduardaSouto28@users.noreply.github.com>
2023-06-15 12:53:27 -0300

    Add files via upload

--- src/main/java/com/example/todoapp/TodoAppApplication.java
+++ src/main/java/com/example/todoapp/TodoAppApplication.java
@@ -2,8 +2,16 @@ package com.example.todoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = {
+		"com.example.todoapp.model"
+})
+@EnableJpaRepositories(basePackages = {
+		"com.example.todoapp.repository"
+})
 public class TodoAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TodoAppApplication.class, args);
